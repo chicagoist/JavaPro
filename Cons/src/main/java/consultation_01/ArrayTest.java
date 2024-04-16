9501d11a28550eb8b381bf765fd7d9a7fd274cf7
chicagoist <chicagoist@exploit.im>
2024-04-16 17:02:38 +0200

    ArrayTest

---
+++ Cons/src/main/java/consultation_01/ArrayTest.java
@@ -0,0 +1,36 @@
+package consultation_01;
+
+public class ArrayTest {
+    public static void main(String[] args){
+
+        // Variant 1
+        int[] numbers = new int[5];
+
+        //Variant 2
+
+
+        // Двумерный
+        int[][] numbers2D = new int[4][5];
+
+        int[][] numbers2Da = new int[7][];
+
+        numbers2Da[0] = new int[3];
+        numbers2Da[1] = new int[6];
+        numbers2Da[2] = new int[9];
+
+        int[][] numbers2Db = {{1,2,3}, {4,5,6}, {7,8,9}};
+
+        for (int[] innerArray : numbers2Db) {
+
+            for(int element:innerArray) {
+        System.out.print(element + " ");
+
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
