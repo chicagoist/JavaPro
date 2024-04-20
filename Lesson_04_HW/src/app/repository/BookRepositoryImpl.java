2007416c6104cd00feba85b41400b7862e844c51
chicagoist <z7t35gbba@mozmail.com>
2024-04-20 20:07:00 +0200

    added class BookRepositoryImpl

---
+++ Lesson_04_HW/src/app/repository/BookRepositoryImpl.java
@@ -0,0 +1,15 @@
+package app.repository;
+
+import app.domain.Book;
+
+public class BookRepositoryImpl implements BookRepository {
+    @Override
+    public Book getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Book getByIsbn(Long isbn) {
+        return null;
+    }
+}
