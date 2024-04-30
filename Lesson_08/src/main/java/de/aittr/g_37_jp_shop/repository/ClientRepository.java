a04455cda924e13dbde79b9aae99c061f35a25fb
chicagoist <le.legioner@gmail.com>
2024-04-30 08:37:19 +0200

    remove @ComponentScan from interface

--- Lesson_08/src/main/java/de/aittr/g_37_jp_shop/repository/ClientRepository.java
+++ Lesson_08/src/main/java/de/aittr/g_37_jp_shop/repository/ClientRepository.java
@@ -1,14 +1,12 @@
 package de.aittr.g_37_jp_shop.repository;
 
 import de.aittr.g_37_jp_shop.domain.entity.Client;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@ComponentScan
 public interface ClientRepository extends JpaRepository<Client,Long> {
 
     @Query(value = "SELECT * FROM client WHERE email = :email",nativeQuery = true)
